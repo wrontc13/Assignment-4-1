a2a9bb1badd971218adb612d9d71896196c770a0
wrontc13 <wrontc13@gmail.com>
2016-04-11 13:10:20 -0400

    Added files via upload

---
+++ Assignment 1.rb
@@ -0,0 +1,8 @@
+message = "welcome "
+name = "Ruby "
+message.capitalize + name
+message.capitalize + name.upcase
+message + name.downcase
+name.reverse + message.capitalize
+message.upcase + name.upcase
+gets
